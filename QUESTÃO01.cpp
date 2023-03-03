a537fef9af308edeca7a5f74132838251265ada8
JOAOPEDRODINIZDEV <115243882+JOAOPEDRODINIZDEV@users.noreply.github.com>
2023-03-03 16:23:46 -0300

    Update QUESTÃO01.cpp

--- QUESTÃO01.cpp
+++ QUESTÃO01.cpp
@@ -10,11 +10,11 @@ int main() {
     int *p;
     p=v;
     for(int j=0; j<5; j++){
-     printf("Digite o %d número:",j+1);
+    printf("Digite o %d número:",j+1);
     scanf("%d",&v[j]);
     }
     for(int p=0; p<5; p++){
-     printf("%p\n",v);
+    printf("O valor %d  é o seu endereço de mémoria %p\n",*(ponteiro+p),(ponteiro+p));
     }
     
     return 0;
